9460276ede62f518fb3422387056f8c2a7a307b3
lauren <moonflower1a@outlook.com>
2016-10-01 04:40:00 -0400

    A small readme update..

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ro-cp
+
+Just a little project to learn PHP don't try using this.
+(Unless you wan't super bad code...)
+:)
---
+++ classes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$sql_set = [ "host"     => "localhost",
+    "port"     => "3306",
+    "db"       => "ragnarok",
+    "user"     => "ragnarok",
+    "password" => "ragnarok" ];
+
+$emu_set = [ "LIp"      => "localhost",
+    "LPort"    => 6900,
+    "CIp"      => "localhost",
+    "CPort"    => 6121,
+    "MIp"      => "localhost",
+    "MPort"    => 5121,
+    "useMD5"   => 1 ];
