4b08d0f5a7cbbb054147c0fbfbe013479181a4d8
Lauren <moonflower1a@outlook.com>
2016-10-04 22:11:08 -0400

    Initial Commit

--- classes/config.php
+++ classes/config.php
@@ -1,20 +1,20 @@
 <?php
-
 class Config {
-
-    public static function  get($path = null) {
-        if($path){
-
-            $config = $GLOBALS['config'];
-            $path = explode('/', $path);
-
-            foreach($path as $bit){
-                if(isset($config[$bit])) {
-                    $config = $config[$bit];
-                }
-            }
-            return $config;
-        }
-        return false;
-    }
+	public static function get($path = null) {
+		if ($path) {
+			$config = $GLOBALS['config'];
+			$path = explode('/', $path);
+			
+			foreach($path as $bit) {
+				if (ISSET($config[$bit])) {
+					$config = $config[$bit];
+				}
+			}
+			
+			return $config;
+		}
+		
+		return false;
+	}
 }
+?>
--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
+require_once("core/init.php");
 
-require_once 'core/init.php';
+if (Session::exists("home")) {
+	echo "<p>";
+	echo Session::flash("home");
+	echo "</p>";
+}
 
-DB::getInstance()->query ("SELECT username FROM users WHERE username = ?", array('alex'));
+$user = new User();
+if ($user->isLoggedIn()) {
+	echo "<P><a href='profile.php?user=".escape($user->data()->username)."'>".escape($user->data()->username)."</a> is logged in</P>";
+	echo "<ul>";
+	echo "<li><a href='changepassword.php'>Change password</a></li>";
+	echo "<li><a href='update.php'>Update profile</a></li>";
+	echo "<li><a href='logout.php'>Log out</a></li>";
+	echo "</ul>";
+} else {
+	echo "You need to <a href='login.php'>log in</a> or <a href='register.php'>register</a>";
+}
+
+if ($user->hasPermission('admin')) {
+	echo "<p>You are an admin!</p>";
+}
+
+if ($user->hasPermission('moderator')) {
+	echo "<p>You are a moderator!</p>";
+}
+?>
