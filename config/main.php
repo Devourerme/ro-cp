fb1aa5f263e8f4f89df3330356b6540a76c2d759
Lauren <moonflower1a@outlook.com>
2016-10-05 10:21:18 -0400

    config update

---
+++ config/main.php
@@ -0,0 +1,8 @@
+<?php
+
+$sql = [
+    'sqlip' => '127.0.0.1:8090', // Host of your MYSQL server localhost | 127.0.0.1:8080
+    'sqldb' => 'ragnarok',
+    'sqlun' => 'ragnarok',
+    'sqlpw' => 'ragnarok'
+];
